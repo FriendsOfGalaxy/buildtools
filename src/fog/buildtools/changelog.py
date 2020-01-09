28c2d348050dde37c62e922b1e96cb8afc8e18d5
FriendsOfGalaxy <FriendsOfGalaxy@gmail.com>
2020-01-09 12:25:12 +0100

    Changelog enhancements

--- src/fog/buildtools/changelog.py
+++ src/fog/buildtools/changelog.py
@@ -1,19 +1,35 @@
 from inspect import cleandoc
-from typing import Dict
+from typing import Dict, Optional
 
 
 class Changelog:
-    def __init__(self, changelog: Dict[str, str]):
+    def __init__(self, changelog: Dict[str, str], check_for_version: Optional[str]=None):
+        if check_for_version and check_for_version not in changelog:
+            raise RuntimeError(f'Changelog for version [{check_for_version}] required')
         self._changelog = changelog
 
-    def __str__(self):
+    def __str__(self) -> str:
         return str(self._changelog)
 
+    def for_version(self, version: str) -> Optional[str]:
+        """Gets changelog text for given `version`.
+        Raises KeyError if no such changelog for `version` found
+        """
+        if type(version) == int:
+            raise TypeError("Version should be str")
+        if version not in self._changelog:
+            raise KeyError(f'No changelog found for version [{version}]')
+
+        cont = cleandoc(self._changelog.get(version, ''))
+        if cont == '':
+            return None
+        return cont
+
     @property
-    def _header(self):
+    def _header(self) -> str:
         return "# Changelog\n\n"
 
-    def to_markdown(self):
+    def to_markdown(self) -> str:
         """Simple implementation - do not support identifiers like [Added] [Fixed]"""
         content = self._header
         for k, v in self._changelog.items():
--- tests/test_changelog.py
+++ tests/test_changelog.py
@@ -1,18 +1,30 @@
 from unittest.mock import patch, mock_open
+import pytest
 
-from src.fog.buildtools import dump_changelog
+from src.fog.buildtools import Changelog
 
 
-def test_changelog():
+def test_changelog_init_check_for_version():
+    with pytest.raises(RuntimeError):
+        Changelog({"0.1": "ver01"}, check_for_version="0.2")
+
+
+def test_changelog_init_check_for_version_empty():
+    with pytest.raises(RuntimeError):
+        Changelog({}, check_for_version="0.2")
+
+
+def test_changelog_init_check_for_version_invalid():
+    with pytest.raises(Exception):
+        Changelog(None, check_for_version="0.2")
+
+
+def test_changelog_to_markdown():
     changelog = {
         "0.1.1": "- fix1\n- fix2",
         "0.1.0": "another log"
     }
-    with patch('builtins.open', mock_open()):
-        with open('CHANGELOG.md', 'r') as f:
-            dump_changelog(changelog, f)
-    f.write.assert_called_once_with(
-"""# Changelog
+    assert Changelog(changelog).to_markdown() == """# Changelog
 
 ## 0.1.1
 - fix1
@@ -22,25 +34,35 @@ def test_changelog():
 another log
 
 """
-    )
 
 
-def test_changelog_dedent():
+def test_changelog_to_markdown_indents():
     changelog = {
         "0.1": """
             - fix1
             - fix2
         """
     }
-    with patch('builtins.open', mock_open()):
-        with open('CHANGELOG.md', 'r') as f:
-            dump_changelog(changelog, f)
-    f.write.assert_called_once_with(
-"""# Changelog
+    assert Changelog(changelog).to_markdown() == """# Changelog
 
 ## 0.1
 - fix1
 - fix2
 
 """
-    )
+
+
+def test_changelog_for_version():
+    changelog = {
+        "0.1": "ver01",
+        "0.2": """
+            ver02
+        """
+    }
+    c = Changelog(changelog)
+    assert c.for_version("0.1") == "ver01"
+    assert c.for_version("0.2") == "ver02"
+    with pytest.raises(TypeError):
+        c.for_version(1)  # int
+    with pytest.raises(KeyError):
+        c.for_version("0.3")
