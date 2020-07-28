a1141e917aee16f83c4a61d0e9a6786de5b06c72
FriendsOfGalaxy <FriendsOfGalaxy@gmail.com>
2020-07-28 12:15:29 +0200

    bump version

--- src/fog/buildtools/version.py
+++ src/fog/buildtools/version.py
@@ -1,6 +1,9 @@
-__version__ = "1.0"
+__version__ = "1.0.1"
 
 __changelog__ = {
+    "1.0.1":"""
+        - update required minimal pip-tools version to support pip 20.1 see https://github.com/open-zaak/open-zaak/issues/585#issuecomment-626666736
+    """,
     "1.0": """
         - Removed dump_changelog
         - Added load_version utility
